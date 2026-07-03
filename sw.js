0494d776bd64fc999b3f658ae7b6eb1c6be7868c
mrd6x3HEQ6exT <chalice-pep-duplex@duck.com>
2026-07-02 21:23:18 -0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'rackroll-v1';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then(resp => {
+      const cp = resp.clone();
+      caches.open(CACHE).then(c => c.put(e.request, cp));
+      return resp;
+    }).catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
+  );
+});
